f6857e571f346250ab20d3e374939bba37060035
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-15 17:34:36 +0400

    Add files via upload

--- pprint_json.py
+++ pprint_json.py
@@ -5,9 +5,9 @@ url = "https://devman.org/media/filer_public/1d/32/\
 1d32132e-efa4-4a6c-bd32-312acc3710ad/alco_shops.json"
 
 
-def write_json(data, filename="answer.json"):
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
+def write_json(data_file, filename="answer.json"):
+    with open(filename, "w") as file_json:
+        json.dump(data_file, file_json, indent=2, ensure_ascii=False)
 
 
 def pretty_json_print(url):
