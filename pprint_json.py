9c35c89e03955f371733814bcdc958efc1472132
nickkorolevv <nick.korolev2011@yandex.ru>
2018-05-15 17:19:05 +0400

    new

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,24 @@
 import json
+import requests
+
+url = "https://devman.org/media/filer_public/1d/32/\
+1d32132e-efa4-4a6c-bd32-312acc3710ad/alco_shops.json"
 
 
-def load_data(filepath):
-    pass
+def write_json(data, filename="answer.json"):
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 
-def pretty_print_json(data):
-    pass
+def pretty_json_print(url):
+    response_object = requests.get(url).json()
+    pretty_print = json.dumps(response_object, indent=2, ensure_ascii=False)
+    return pretty_print
 
 
-if __name__ == '__main__':
-    pass
+def main():
+    print(pretty_json_print(url))
+
+
+if __name__ == "__main__":
+    main()
