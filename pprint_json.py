cc373c4903f7bf017e4f70cd6d405a0033fcf50a
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-16 23:25:10 +0400

    Создана функция ввода директории

--- pprint_json.py
+++ pprint_json.py
@@ -1,19 +1,29 @@
 import json
+import os
 
 
-def open_json(filepath="alco_shops.json"):
+def get_filepath():
+    way_to_file = os.path.abspath(os.curdir) + "\\"
+    filepath = way_to_file + input("Введите название файла \n")
+    return filepath
+
+
+def load_data():
+    filepath = get_filepath()
     with open (filepath, 'r', encoding='utf-8') as file_handler:
         return json.load(file_handler)
 
+
 def pretty_json_print():
-    pretty_print = json.dumps(open_json(), indent=2, ensure_ascii=False)
+    data_file = load_data()
+    pretty_print = json.dumps(data_file, indent=2, ensure_ascii=False)
     print(pretty_print)
 
 
 def main():
     pretty_json_print()
 
-    
+
 if __name__ == "__main__":
     main()
 
