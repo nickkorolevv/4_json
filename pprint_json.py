22b0f5386956b4ed4ee72fdd36a14d12f8d22355
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-16 15:02:07 +0400

    Add files via upload

--- pprint_json.py
+++ pprint_json.py
@@ -1,20 +1,19 @@
 import json
-import requests
 
-def get_json():
-    data_file = "your_json_file"
-    response_object = requests.get(data_file).json()
-    return response_object
 
+def get_json(filepath="alco_shops.json"):
+    with open (filepath, 'r', encoding='utf-8') as file_handler:
+        return json.load(file_handler)
 
 def pretty_json_print():
     pretty_print = json.dumps(get_json(), indent=2, ensure_ascii=False)
-    return pretty_print
+    print(pretty_print)
 
 
 def main():
-    print(pretty_json_print())
+    pretty_json_print()
 
     
 if __name__ == "__main__":
     main()
+
