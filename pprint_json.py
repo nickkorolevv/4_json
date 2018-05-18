7f7beecde6da287a08ca54920fb6b537d2986714
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-18 18:34:10 +0400

    исправлен ввод файла

--- pprint_json.py
+++ pprint_json.py
@@ -3,24 +3,20 @@ import sys
 
 
 def load_data(filepath):
-    with open (filepath, 'r', encoding='utf-8') as file_handler:
+    with open (filepath, "r", encoding="utf-8") as file_handler:
         return json.load(file_handler)
 
 
-def pretty_json_print(filename):
-    pretty_print = json.dumps(filename, indent=2, ensure_ascii=False)
+def pretty_json_print(decoded_json):
+    pretty_print = json.dumps(decoded_json, indent=2, ensure_ascii=False)
     print(pretty_print)
-
-
-def main():
-    dataname = load_data(filepath)
-    pretty_json_print(dataname)
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         filepath = format(sys.argv[1])
     else:
-        filepath = input("Укажите путь до json файла: ")
-    main()
+        exit("Файл не выбран, пожалуйста выберите файл")
+    decoded_json = load_data(filepath)
+    pretty_json_print(decoded_json)
 
